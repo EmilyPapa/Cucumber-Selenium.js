b99b4d5b7fc2d922aa433e080b9f0f6437f2c731
EmilyPapa <emilypap97s@gmail.com>
2021-06-04 11:26:14 +0300

    few minor changes

--- features/hooks.js
+++ features/hooks.js
@@ -3,7 +3,6 @@ var {After, Before} = require('@cucumber/cucumber');
 Before(function(){
     return this.driver.manage().window().maximize();
 });
-After(function(){
-    return this.driver.quit();
-
-});
+//After(function(){
+// return this.driver.quit();
+//});
